97b616c3544cbf29cf4c8296d18303c5f4b5d1b1
heatherl42 <heather.lawson@student.uhasselt.be>
2023-04-18 22:32:33 +0200

    Exercises 9 and 10

---
+++ src/routes/02_exercises/01_web_technologies/exercise_09/Exercise.svelte
@@ -0,0 +1,10 @@
+<script>
+const pi = 3.14
+let radius = 2
+
+function circumference(radius) {
+    return 2*pi*radius;
+}
+</script>
+
+<p>The circumference of a circle with radius {radius}m is {circumference(2)}m.</p>
---
+++ src/routes/02_exercises/01_web_technologies/exercise_10/Exercise.svelte
@@ -0,0 +1,7 @@
+<script>
+import Exercise3 from '../exercise_03/Exercise.svelte'
+import Exercise2 from '../exercise_02/Exercise.svelte'
+</script>
+
+<div><Exercise3 /></div>
+<div><Exercise2 /></div>
