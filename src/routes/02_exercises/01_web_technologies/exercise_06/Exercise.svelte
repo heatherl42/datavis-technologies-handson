0be09ff238e2c8b57b53dd0992b656324283d6f1
heatherl42 <heather.lawson@student.uhasselt.be>
2023-04-18 21:12:10 +0200

    Exercises 1-6

--- src/routes/02_exercises/01_web_technologies/exercise_06/Exercise.svelte
+++ src/routes/02_exercises/01_web_technologies/exercise_06/Exercise.svelte
@@ -1,1 +1,9 @@
-I really like data visualisation!
+<style>
+  p {
+    color: steelblue;
+    font-size: larger;
+    font-family: Calibri;
+  }
+</style>
+
+<p>I really like data visualisation!</p>
