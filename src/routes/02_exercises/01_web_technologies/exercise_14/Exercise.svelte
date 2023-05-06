721671e0ccb213387380823c6ea5349e4b6e88fd
heatherl42 <heather.lawson@student.uhasselt.be>
2023-05-06 15:05:09 +0200

    Exercise 14

--- src/routes/02_exercises/01_web_technologies/exercise_14/Exercise.svelte
+++ src/routes/02_exercises/01_web_technologies/exercise_14/Exercise.svelte
@@ -2,15 +2,21 @@
   let current = "";
 </script>
 
+<style>
+  .selected {
+    background-color:blue
+  }
+</style>
+
 <!-- We will cover buttons later on! -->
-<button class="btn btn-secondary" on:click={() => (current = "messi")}>
+<button class="btn btn-secondary {current === "messi" ? 'selected': ''}" on:click={() => (current = "messi")}>
   Messi
 </button>
 
-<button class="btn btn-secondary" on:click={() => (current = "ronaldo")}>
+<button class="btn btn-secondary {current ==="ronaldo" ? 'selected': ''}" on:click={() => (current = "ronaldo")}>
   Ronaldo
 </button>
 
-<button class="btn btn-secondary" on:click={() => (current = "neymar")}>
+<button class="btn btn-secondary {current ==="neymar" ? 'selected': ''}" on:click={() => (current = "neymar")}>
   Neymar
 </button>
